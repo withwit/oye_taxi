f493cc8d21f2c0fa62a5fd1d77adaffc3f117f12
Heisenberg <anmolsahu007.as@gmail.com>
2021-02-09 20:53:57 +0530

    map and markes added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
 import "./App.css";
+import { useState } from "react";
+import ReactMapGL, { Marker } from "react-map-gl";
+import * as data from "./data/data.json";
+import MARKER from "./taxi.png";
+const TOKEN =
+  "pk.eyJ1IjoiZHdpZ2h0c2NocnV0ZSIsImEiOiJja2t4d2Z3bXUybzB6MnVzMW9zbXhqdmtyIn0.843lElK1D2yFrx7TzOBiwA";
 
 function App() {
+  const [viewport, setviewport] = useState({
+    latitude: 45.4211,
+    longitude: -75.6903,
+    width: "100vw",
+    height: "100vh",
+    zoom: 11,
+  });
+
   return (
     <div className="App">
-      <h1>Hia</h1>
+      <ReactMapGL
+        {...viewport}
+        mapboxApiAccessToken={TOKEN}
+        //mapStyle="mapbox://styles/dwightschrute/ckky3wbf229rt17oartcj7zli"
+        onViewportChange={(viewport) => {
+          setviewport(viewport);
+        }}
+      >
+        {data.features.map((park) => (
+          <Marker
+            key={park.properties.PARK_ID}
+            latitude={park.geometry.coordinates[1]}
+            longitude={park.geometry.coordinates[0]}
+          >
+            <button className="marker_btn">
+              <img src={MARKER} alt="taxi" />
+            </button>
+          </Marker>
+        ))}
+      </ReactMapGL>
     </div>
   );
 }
